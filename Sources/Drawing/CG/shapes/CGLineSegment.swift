c763c2088ec4af425840df3ca88c8a8d4e63bdfa
milos <milos@screensailor.net>
2020-05-02 09:28:19 +0100

    improve CGLineSegement.intersection(with:) to handle vertical lines

--- Sources/Drawing/CG/shapes/CGLineSegment.swift
+++ Sources/Drawing/CG/shapes/CGLineSegment.swift
@@ -56,24 +56,25 @@ extension CGLineSegment {
         point.applying(reflection())
     }
 
-    public func reflection() -> CGAffineTransform {
-        guard let s = slope().ifFinite else {
-            return CGAffineTransform(
-                a: -1, b: 0,
-                c: 0, d: 1,
-                tx: 2 * end.x, ty: -0.5
-            )
+    public func intersection(with other: CGLineSegment) -> CGPoint? {
+        let (a, b, c, d) = (slope(), yIntercept(), other.slope(), other.yIntercept())
+        let x: CGFloat, s: CGFloat, i: CGFloat
+        switch (a.isFinite, c.isFinite) {
+        case (false, false): return nil
+        case (true, true): (x, s, i) = ((d - b) / (a - c), a, b)
+        case (false, true): (x, s, i) = (start.x, c, d)
+        case (true, false): (x, s, i) = (other.start.x, a, b)
         }
-        let c = yIntercept()
-        let a = 1 - s * s
-        let b = 1 + s * s
-        let m1 = a / b
-        let m2 = 2 * s / b
-        return CGAffineTransform(
-            a: m1, b: m2,
-            c: m2, d: -m1,
-            tx: -2 * s * c / b, ty: 2 * c / b
-        )
+        let y = s * x + i
+        guard
+            x.isFinite, y.isFinite,
+            x.isBetween(start.x, and: end.x),
+            y.isBetween(start.y, and: end.y),
+            x.isBetween(other.start.x, and: other.end.x),
+            y.isBetween(other.start.y, and: other.end.y)
+            else
+        { return nil }
+        return .init(x: x, y: y)
     }
 }
 
